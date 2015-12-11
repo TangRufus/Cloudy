c5625bdcdae68b74941f1ab3ed0fe308067e0250
Tang Rufus <tangrufus@gmail.com>
2015-12-11 14:16:38 +0800

    cleaning up

--- app/actions/loginActions.js
+++ app/actions/loginActions.js
@@ -7,27 +7,6 @@ export function onFormFieldChange(field, value) {
   };
 }
 
-function formRequest() {
-  return {
-    type: loginTypes.FORM_REQUEST
-  };
-}
-
-function tryLogin(email, apiKey) {
-  return fetch('https://ssswww.google.com/search?q=secret+sauce');
-}
-
-function success() {console.log('success'); }
-
-function handleFormSubmit() {
-  return dispatch => {
-    dispatch(formRequest())
-    return tryLogin().then(
-      data => success()
-     );
-  };
-}
-
 export function onFormSubmit() {
   return {
     type: loginTypes.FORM_REQUEST
