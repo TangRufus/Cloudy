978e56f2f4d0e34ea5904f0f4823c1f3e3036087
Tang Rufus <tangrufus@gmail.com>
2015-12-14 05:34:16 +0800

    Add router & launch scene

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react-native';
+import React, {
+  Component,
+  StyleSheet
+} from 'react-native';
+import { Router, Route } from 'react-native-router-flux';
 import Login from './Login';
+import Launch from './Launch';
+
+const styles = StyleSheet.create({
+  scene: {
+    top: 64
+  }
+});
 
 export default class App extends Component {
+  // @TODO: Add schemas
   render() {
     return (
-      <Login />
+      <Router hideNavBar>
+        <Route initial name="launch" component={Launch} title="Launch"/>
+          <Route name="login">
+              <Router sceneStyle={styles.scene}>
+                  <Route name="loginModal" component={Login} title="Login"/>
+              </Router>
+          </Route>
+        </Router>
     );
   }
 }
---
+++ app/containers/Launch.js
@@ -0,0 +1,28 @@
+import React, {
+  Component,
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  }
+});
+
+export default class Launch extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Launch page</Text>
+        <Button onPress={Actions.login}>Go to Login page</Button>
+      </View>
+    );
+  }
+}
