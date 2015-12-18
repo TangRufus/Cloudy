ff22fa4e691bae88b7c66a92d062ea65f079e7f3
Tang Rufus <tangrufus@gmail.com>
2015-12-18 16:53:54 +0800

    Router: Add sceneStyle

--- app/containers/AppRouter.js
+++ app/containers/AppRouter.js
@@ -1,11 +1,18 @@
 import React, {
   Component,
-  PropTypes
+  PropTypes,
+  StyleSheet
 } from 'react-native';
 import { Router, Route } from 'react-native-router-flux';
 import Launch from './Launch';
 import Login from './Login';
 import Blank from './Blank';
+
+const styles = StyleSheet.create({
+  scene: {
+    top: 64
+  }
+});
 
 export default class AppRouter extends Component {
   static propTypes = {
@@ -19,13 +26,13 @@ export default class AppRouter extends Component {
       <Router hideNavBar>
           <Route initial name="launch" component={Launch} title="Launch" wrapRouter />
           <Route name="login">
-            <Router>
+            <Router sceneStyle={styles.scene}>
               <Route name="loginForm" component={Login} title="Login" />
             </Router>
           </Route>
 
           <Route name="blank">
-            <Router>
+            <Router sceneStyle={styles.scene}>
               <Route name="blank1" renderLeftButton={renderLeftButton} component={Blank} title="Blank" />
             </Router>
           </Route>
